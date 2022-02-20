a0bc91918db92a279ccc137de88ea5deb5818712
El ghozzi wael <91761538+waelelghozzi@users.noreply.github.com>
2022-02-20 16:52:37 +0100

    Delete js_bases_exo10_1.js

--- js_bases_exo10_1.js
+++
@@ -1,8 +0,0 @@
-//i used node.js to get the output.. no html needed
-let v="chanter";
-let ac="eur";
-let acteur= v.replace("er",ac);
-let ver=v.substr(0,5)+"e";
-
-let text=v+" : "+" le "+acteur+" "+ver;
-console.log(text);
